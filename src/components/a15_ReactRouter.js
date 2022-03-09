a6c6f44f19eec4690fa1d9876044a0da222eed90
Mirko Lamberti <mirkolamberti@gmail.com>
2022-03-08 21:58:43 -0300

    aula 14 sobre state lift

---
+++ src/components/a15_ReactRouter.js
@@ -0,0 +1,8 @@
+/**
+ * aula 15 - React Router
+ * Rotas para gerenciar diversas páginas
+ * É possível trocar só uma parte do layout (view)
+ * É um pacote externo que precisa ser instalado
+ */
+
+// npm install react-router-dom
